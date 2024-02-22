4118da61602b28afc0bff84d254532c6c14a34a7
islambahaaa <bossislam55@gmail.com>
2024-02-22 07:28:53 +0200

    create edit note view

--- lib/widgets/note_item.dart
+++ lib/widgets/note_item.dart
@@ -1,57 +1,65 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:notes_app/views/edit_note_view.dart';
 
 class NoteItem extends StatelessWidget {
   const NoteItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(top: 20, bottom: 20, left: 9),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(16),
-        color: Color(0xff3b5a94),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-              title: const Text(
-                'Flutter Tips',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 24,
-                ),
-              ),
-              subtitle: Padding(
-                padding: const EdgeInsets.symmetric(vertical: 16),
-                child: Text(
-                  'Build your career with Islam bahaa',
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(builder: (context) {
+          return const EditNoteView();
+        }));
+      },
+      child: Container(
+        padding: const EdgeInsets.only(top: 20, bottom: 20, left: 9),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          color: Color(0xff3b5a94),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+                title: const Text(
+                  'Flutter Tips',
                   style: TextStyle(
-                    color: Colors.black.withOpacity(0.4),
-                    fontSize: 20,
+                    color: Colors.black,
+                    fontSize: 24,
                   ),
                 ),
-              ),
-              trailing: IconButton(
-                onPressed: () {},
-                icon: const Icon(
-                  FontAwesomeIcons.trash,
-                  size: 25,
-                  color: Colors.black,
+                subtitle: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 16),
+                  child: Text(
+                    'Build your career with Islam bahaa',
+                    style: TextStyle(
+                      color: Colors.black.withOpacity(0.4),
+                      fontSize: 20,
+                    ),
+                  ),
                 ),
-              )),
-          Padding(
-            padding: const EdgeInsets.only(right: 24),
-            child: Text(
-              'Feb 18,2024',
-              style: TextStyle(
-                color: Colors.black.withOpacity(0.4),
-                fontSize: 17,
+                trailing: IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    FontAwesomeIcons.trash,
+                    size: 25,
+                    color: Colors.black,
+                  ),
+                )),
+            Padding(
+              padding: const EdgeInsets.only(right: 24),
+              child: Text(
+                'Feb 18,2024',
+                style: TextStyle(
+                  color: Colors.black.withOpacity(0.4),
+                  fontSize: 17,
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
