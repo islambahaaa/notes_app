046203612db52e4da4c70fd37d9dabe4ede77560
islambahaaa <bossislam55@gmail.com>
2024-02-22 09:51:51 +0200

    create add note cubit

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:notes_app/constants.dart';
-import 'package:notes_app/widgets/add_note_modal_bottom_sheet.dart';
-import 'package:notes_app/widgets/notes_view_body.dart';
+import 'package:notes_app/views/widgets/add_note_modal_bottom_sheet.dart';
+import 'package:notes_app/views/widgets/notes_view_body.dart';
 
 class NotesView extends StatelessWidget {
   const NotesView({super.key});
